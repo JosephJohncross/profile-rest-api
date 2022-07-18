56cf7383baa45333cdb239fa0e5572c713358c52
Joseph Ibochi <josephibochi@gmail.com>
2022-07-18 23:01:39 +0100

    Added user profile feed api

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from profiles_api import models
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 
 
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly #checks if user is authenticated or perform read only for returned viewsets
+from rest_framework.permissions import IsAuthenticated #checks if user is authenticated only returned viewsets
 
 from profiles_api import serializers
 from profiles_api import models
@@ -114,6 +116,22 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 
 class UserLoginApiView(ObtainAuthToken):
-    """Handles creating user authemtication tokens"""
+    """Handles creating user authentication tokens"""
 
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        # IsAuthenticatedOrReadOnly,
+        IsAuthenticated,
+
+    )
+    
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
